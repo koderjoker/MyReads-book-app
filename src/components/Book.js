ff4706ecefe4a040a3ba2a6bcbf8425da9a51fcd
Koder Joker <koderjoker@gmail.com>
2018-12-12 17:25:56 +0530

    Add error handling for exceptions in data

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,10 +9,10 @@ class Book extends React.Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${ this.props.book.imageLinks.thumbnail }")`}}></div>
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${ this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ""}")`}}></div>
             <div className="book-shelf-changer">
               {/*Shelf value sent as book prop for shelf status. If value is changed call changeShelf and send book and new select menu value as params*/}
-              <select value={ this.props.book.shelf } onChange={(event) => {this.props.changeShelf(this.props.book, event.target.value);}}>
+              <select value={this.props.book.shelf ? this.props.book.shelf : 'none'} onChange={(event) => {this.props.changeShelf(this.props.book, event.target.value);}}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -21,8 +21,8 @@ class Book extends React.Component {
               </select>
             </div>
           </div>
-          <div className="book-title">{ this.props.book.title }</div>
-          <div className="book-authors">{ this.props.book.authors }</div>
+          <div className="book-title">{ this.props.book.title ? this.props.book.title: 'Untitled'}</div>
+          <div className="book-authors">{ this.props.book.authors ? this.props.book.authors.join(', ') : 'No author listed'}</div>
         </div>
       </li>
     );
