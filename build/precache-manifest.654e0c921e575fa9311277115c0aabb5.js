da9686265f036eaea49dd9412f7a8d01dd827d46
Koder Joker <koderjoker@gmail.com>
2018-12-25 11:23:05 +0530

    Host website

---
+++ build/precache-manifest.654e0c921e575fa9311277115c0aabb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-book-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads-book-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-book-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "b0a40440d91a1a9f16f9",
+    "url": "/MyReads-book-app/static/js/runtime~main.b0a40440.js"
+  },
+  {
+    "revision": "fbcd540cc1df87dac142",
+    "url": "/MyReads-book-app/static/js/main.fbcd540c.chunk.js"
+  },
+  {
+    "revision": "2cce8bce1daf80bd16b5",
+    "url": "/MyReads-book-app/static/js/1.2cce8bce.chunk.js"
+  },
+  {
+    "revision": "fbcd540cc1df87dac142",
+    "url": "/MyReads-book-app/static/css/main.6aded527.chunk.css"
+  },
+  {
+    "revision": "56fc0c2315e153573fc16388a0431948",
+    "url": "/MyReads-book-app/index.html"
+  }
+];
